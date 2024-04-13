64b70bc312b117eb3074d6404eb62e4da08a515b
dav1601 <davigithub16@gmail.com>
2024-04-14 05:05:54 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -28,15 +28,15 @@ export default defineConfig({
             },
         }),
     ],
-    // build: {
-    //     rollupOptions: {
-    //         output: {
-    //             entryFileNames: `assets/[name].js?` ,
-    //             chunkFileNames: `assets/[name].js?` ,
-    //             assetFileNames: `assets/[name].[ext]`,
-    //         },
-    //     },
-    // },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name]-[hash].js` ,
+                chunkFileNames: `assets/[name]-[hash].js` ,
+                assetFileNames: `assets/[name]-[hash].[ext]`,
+            },
+        },
+    },
     resolve: {
         alias: {
             "@": "/resources/js",
